499d0033b1bf6584374be8164382b5a6f31804a5
Abolfazl Banaei <abolfazlbanaei85@gmail.com>
2026-05-09 11:23:16 +0330

    the auth provider file has been updated

--- src/provider/AuthProvider.js
+++ src/provider/AuthProvider.js
@@ -18,9 +18,29 @@ function AuthProvider({ children, page }) {
 
   if (isPending)
     return (
-      <p className=" m-auto bg-blue-100 text-blue-500 w-fit animate-ping">
-        Loading...
-      </p>
+      <div className="flex items-center justify-center min-h-[40vh]">
+        <div className="gradient-text">در حال بارگذاری...</div>
+
+        <style jsx>{`
+          .gradient-text {
+            font-size: 1.8rem;
+            font-weight: bold;
+            background: linear-gradient(90deg, #3b82f6, #a855f7, #22d3ee);
+            background-size: 300%;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            animation: moveGradient 2s linear infinite;
+          }
+          @keyframes moveGradient {
+            0% {
+              background-position: 0%;
+            }
+            100% {
+              background-position: 300%;
+            }
+          }
+        `}</style>
+      </div>
     );
 
   return children;
